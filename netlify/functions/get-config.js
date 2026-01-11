07a2f389215328c201d55ddaf993fd69a74ba17e
Sisko25 <64239798+Sisko25@users.noreply.github.com>
2026-01-11 13:20:37 +0800

    Update get-config.js

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -1,14 +1,23 @@
 // netlify/functions/get-config.js
+
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      DEEPSEEK_KEY: process.env.DEEPSEEK_KEY,
-      POLYGON_KEY: process.env.POLYGON_KEY
-    })
-  };
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      // CHANGED: Sending TWELVEDATA_KEY instead of POLYGON_KEY
+      body: JSON.stringify({
+        DEEPSEEK_KEY: process.env.DEEPSEEK_KEY,
+        TWELVEDATA_KEY: process.env.TWELVEDATA_KEY
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
 };
